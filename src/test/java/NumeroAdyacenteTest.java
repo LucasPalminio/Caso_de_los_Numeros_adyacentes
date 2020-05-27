95e8dd14610760cb287cd104f13ac80868a1f1ef
LucasPalminio <l.palminio01@ufromail.cl>
2020-05-27 09:35:37 -0400

    first commit

---
+++ src/test/java/NumeroAdyacenteTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class NumeroAdyacenteTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
