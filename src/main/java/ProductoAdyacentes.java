3164b3f331f47628e9f05b47f3bf036c71ba359b
LucasPalminio <l.palminio01@ufromail.cl>
2020-05-27 10:53:50 -0400

    Paso 1 de la actividad Implementar el pseudocodigo en Java y las pruebas unitarias

--- src/main/java/ProductoAdyacentes.java
+++ src/main/java/ProductoAdyacentes.java
@@ -1,3 +1,13 @@
 public class ProductoAdyacentes {
-
+    public static int maximoProductoAdyacente(int[] arreglo){
+        //verificarArreglo(arreglo); //se ve a futuro
+        int productoMayor = arreglo[0]*arreglo[1];
+        for(int i = 0; i < arreglo.length - 1; i++){
+            int producto = arreglo[i] * arreglo[i+1];
+            if( producto > productoMayor){
+                productoMayor = producto;
+            }
+        }
+        return productoMayor;
+    }
 }
--- src/test/java/ProductoAdyacentesTest.java
+++ src/test/java/ProductoAdyacentesTest.java
@@ -1,7 +1,15 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 public class ProductoAdyacentesTest {
-
+    public static ProductoAdyacentes p1;
+    @BeforeClass
+    public static void beforeClass(){
+        p1 = new ProductoAdyacentes();
+    }
+    @AfterClass
+    public static void AfterClass(){
+        p1 = null;
+    }
     @Before
     public void setUp() throws Exception {
 
@@ -12,4 +20,11 @@ public class ProductoAdyacentesTest {
 
     }
 
+    @Test
+    public void TestMaximoProductoAdyacente() {
+        int[] arreglo = {1, -4, 2, 2, 5, -1};
+        int esperado = 10;
+        int real = p1.maximoProductoAdyacente(arreglo);
+        assertEquals(esperado,real);
+    }
 }
