0625e636684039aa1271a7d575d8cd6654d81e4f
LucasPalminio <l.palminio01@ufromail.cl>
2020-06-01 17:12:56 -0400

    Uograde al codigo 2.0 Le agregamos sobrecarga de metodos para arreglo de tipo String, char, int, double Ademas un contador de cuantos numeros hay en un arreglo de String

--- src/main/java/ProductoAdyacentes.java
+++ src/main/java/ProductoAdyacentes.java
@@ -1,44 +1,92 @@
 public class ProductoAdyacentes {
-    public static double maximoProductoAdyacente(double[] arreglo){
-        verificarArreglo(arreglo); //se ve a futuro
-        double productoMayor = arreglo[0]*arreglo[1]; //Inicializamos la variable productoMayor
-        //con el primer producto adyacente
+    public static double maximoProductoAdyacente(double[] arreglo) {
+        if (arreglo.length > 1) { //El largo del arreglo debe ser mayor que 1
 
-        for(int i = 0; i < arreglo.length - 1; i++){ //Recorremos el arreglo hasta encontra el producto mayor
-            //el indice (i) comienza desde la primera posicion (0)
-            //hasta llegar a la penultima posicion ( largo - 2)
+            double productoMayor = arreglo[0] * arreglo[1]; //Inicializamos la variable productoMayor
+            //con el primer producto adyacente
 
-            //Realizamos uno por uno el producto adyacente,
+            for (int i = 0; i < arreglo.length - 1; i++) { //Recorremos el arreglo hasta encontra el producto mayor
+                //el indice (i) comienza desde la primera posicion (0)
+                //hasta llegar a la penultima posicion ( largo - 2)
 
-            double producto = arreglo[i] * arreglo[i+1];//El producto se realiza entre el elemento que esta
-            //en la posicion i y en la posicion i+1 (el siguiente)
+                //Realizamos uno por uno el producto adyacente,
 
-            if( producto > productoMayor){//Si: el producto actual es mayor que el producto mayor, entonces:
+                double producto = arreglo[i] * arreglo[i + 1];//El producto se realiza entre el elemento que esta
+                //en la posicion i y en la posicion i+1 (el siguiente)
 
-                //este producto actual lo guardamos en la variable productoMayor
-                productoMayor = producto;
+                if (producto > productoMayor) {//Si: el producto actual es mayor que el producto mayor, entonces:
 
-            }//De lo contrario: no se hace nada
+                    //este producto actual lo guardamos en la variable productoMayor
+                    productoMayor = producto;
+
+                }//De lo contrario: no se hace nada
+            }
+            //Una vez realizado el algoritmo,
+            //retornamos el valor del productoMayor del arreglo
+            return productoMayor;
+        } else if (arreglo.length == 1) {
+            return arreglo[0]; //Si el arreglo tiene un solo elemento, retornamos solamente ese elemento
+        } else {
+            return 0; //Si el arreglo no tiene elementos, retornamos un 0
         }
-        //Una vez realizado el algoritmo,
-        //retornamos el valor del productoMayor del arreglo
-        return productoMayor;
     }
-    public static int maximoProductoAdyacente(int[] arreglo){
+
+    public static int maximoProductoAdyacente(int[] arreglo) {
+        //SobreCarga del metodo maximoProductoAdyacente para arreglo de tipo int
         double[] arregloDouble = new double[arreglo.length];
+        //Creamos un arreglo double con la misma dimension del arreglo int
         for (int i = 0; i < arreglo.length; i++) {
+            //Recorremos el arreglo int para guardar cada elemento al arreglo double
             arregloDouble[i] = (double) arreglo[i];
         }
         return (int) maximoProductoAdyacente(arregloDouble);
+        //Llamamos al metodo original, le pasamos el arreglo double, nos devuelve el resultado en double
+        // y con un cast lo transformamos a int
 
     }
 
-    public static void verificarArreglo(double[] arreglo){
-        if (arreglo.length<=1){
-            System.out.println("El largo del arreglo es menor o igual que 1, no podemos ejecutar calcular el producto adyacente");
-            System.exit(-1);
-        }
 
+    public static double maximoProductoAdyacente(String[] arregloString) {
+        //Primero contar cuantos numeros hay en el arreglo
+        int cantidadNumeros = contarNumerosEnUnArregloString(arregloString);
+        //Con este numero, le damos un tamaño a nuestro vector double
+        double[] arregloDouble = new double[cantidadNumeros];
+
+        //Luego cada string que haya en el arreglo original, lo transformamos a double.
+        // Si hay un string no numerico este se lo salta, gracias a que captura el try-catch
+        for (int i = 0, j = 0; i < arregloString.length; i++, j++) {
+            try {
+                arregloDouble[j] = Double.parseDouble(arregloString[i]);
+            } catch (NumberFormatException e) {
+                //System.out.println(e);
+                j--;
+                continue;
+            }
+        }
+        return maximoProductoAdyacente(arregloDouble);
+
+    }
+
+    public static double maximoProductoAdyacente(char[] arregloChar) {
+        String[] arregloString = arregloChar.toString().split(""); //Transforma el arreglo de char a arreglo string
+        return maximoProductoAdyacente(arregloString); //Le pasamos el arreglo String al metodo que tambien es sobrecarga de maximoProductoAdyacente
+
+
+    }
+
+    public static int contarNumerosEnUnArregloString(String[] arregloOriginal) {
+        //Cuenta cuanto numeros hay en un arreglo de tipo string
+        int cantidadNumeros = 0;
+        for (int i = 0; i < arregloOriginal.length; i++) {
+            try {
+                Double.parseDouble(arregloOriginal[i]);//Si se opera esta instruccion correctamente, entonces la cadena es un numero
+                cantidadNumeros++; //Aumenta la cantidad en uno
+            } catch (NumberFormatException e) {
+
+                continue;//Si salto un error, quiere decir que la cadena no es numerico, por lo tanto pasamos al sgte elemento
+            }
+        }
+        return cantidadNumeros;
 
     }
 }
--- src/test/java/ProductoAdyacentesTest.java
+++ src/test/java/ProductoAdyacentesTest.java
@@ -1,15 +1,20 @@
 import static org.junit.Assert.*;
+
 import org.junit.*;
+
 public class ProductoAdyacentesTest {
     public static ProductoAdyacentes p1;
+
     @BeforeClass
-    public static void beforeClass(){
+    public static void beforeClass() {
         p1 = new ProductoAdyacentes();
     }
+
     @AfterClass
-    public static void AfterClass(){
+    public static void AfterClass() {
         p1 = null;
     }
+
     @Before
     public void setUp() throws Exception {
 
@@ -19,29 +24,32 @@ public class ProductoAdyacentesTest {
     public void tearDown() throws Exception {
 
     }
+
     @Test
-    public void TestTestMaximoProductoAdyacente_Positivo1(){
-        int[] arreglo = { 9,2,3};
+    public void TestTestMaximoProductoAdyacente_Positivo1() {
+        int[] arreglo = {9, 2, 3};
         int esperado = 18;
         int real = p1.maximoProductoAdyacente(arreglo);
-        assertEquals(esperado,real);
+        assertEquals(esperado, real);
     }
 
     @Test
     public void TestMaximoProductoAdyacente_Positivo2() {
-        int[] arreglo = {3,4,9,8,9};
+        int[] arreglo = {3, 4, 9, 8, 9};
         int esperado = 72;
         int real = p1.maximoProductoAdyacente(arreglo);
-        assertEquals(esperado,real);
+        assertEquals(esperado, real);
     }
+
     @Test
-    public void TestProductoAdyacente_Negativo1(){
-        int[] arreglo= {-1,3,-4,2};
+    public void TestProductoAdyacente_Negativo1() {
+        int[] arreglo = {-1, 3, -4, 2};
         int esperado = -3;
         int real = p1.maximoProductoAdyacente(arreglo);
-        assertEquals(esperado,real);
+        assertEquals(esperado, real);
 
     }
+
     @Test
     public void TestProductoAdyacente_Negativo2() {
         int[] arreglo = {-9, 2, -3};
@@ -49,23 +57,26 @@ public class ProductoAdyacentesTest {
         int real = p1.maximoProductoAdyacente(arreglo);
         assertEquals(esperado, real);
     }
+
     @Test
-    public void TestProductoAdyacente_Cero1(){
-        int[] arreglo = {-1,0,3,-5};
+    public void TestProductoAdyacente_Cero1() {
+        int[] arreglo = {-1, 0, 3, -5};
         int esperado = 0;
         int real = p1.maximoProductoAdyacente(arreglo);
         assertEquals(esperado, real);
     }
+
     @Test
-    public void TestProductoAdyacente_Cero2(){
-        int[] arreglo = {0,0,1,0};
+    public void TestProductoAdyacente_Cero2() {
+        int[] arreglo = {0, 0, 1, 0};
         int esperado = 0;
         int real = p1.maximoProductoAdyacente(arreglo);
         assertEquals(esperado, real);
     }
+
     @Test
-    public void TestProductoAdyacente_Numeros_Iguales1(){
-        int[] arreglo = {2,2,2};
+    public void TestProductoAdyacente_Numeros_Iguales1() {
+        int[] arreglo = {2, 2, 2};
         int esperado = 4;
         int real = p1.maximoProductoAdyacente(arreglo);
         assertEquals(esperado, real);
@@ -73,22 +84,24 @@ public class ProductoAdyacentesTest {
     }
 
     @Test
-    public void TestProductoAdyacente_Numeros_Iguales2(){
-        int[] arreglo = {0,0,0,0,0,0};
+    public void TestProductoAdyacente_Numeros_Iguales2() {
+        int[] arreglo = {0, 0, 0, 0, 0, 0};
         int esperado = 0;
         int real = p1.maximoProductoAdyacente(arreglo);
         assertEquals(esperado, real);
 
     }
-// Aquí empiezan los test de los posibles errores.
+
+    // Aquí empiezan los test de los posibles errores.
     @Test
-        public void TestProductoAdyacente_error_largo_uno1() {
-        int[] arreglo = {1};
-        int esperado = 1;
+    public void TestProductoAdyacente_error_largo_uno1() {
+        int[] arreglo = {9};
+        int esperado = 9;
         int real = p1.maximoProductoAdyacente(arreglo);
         assertEquals(esperado, real);
 
     }
+
     @Test
     public void TestProductoAdyacente_error_largo_uno2() {
         int[] arreglo = {};
@@ -97,28 +110,31 @@ public class ProductoAdyacentesTest {
         assertEquals(esperado, real);
 
     }
+
     @Test
-    public void TestProductoAdyacente_error_numerodecimal1(){
-        double[] arreglo = {1.2,3.1,2.4,1};
+    public void TestProductoAdyacente_error_numerodecimal1() {
+        double[] arreglo = {1.2, 3.1, 2.4, 1};
         double esperado = 7.44;
         double real = p1.maximoProductoAdyacente(arreglo);
-        assertEquals(esperado, real);
+        assertEquals(esperado, real, 0.001);
 
     }
+
     @Test
-    public void TestProductoAdyacente_error_numerodecimal2(){
-        double[] arreglo = {3.14,1,2};
+    public void TestProductoAdyacente_error_numerodecimal2() {
+        double[] arreglo = {3.14, 1, 2};
         double esperado = 3.14;
         double real = p1.maximoProductoAdyacente(arreglo);
-        assertEquals(esperado, real);
+        assertEquals(esperado, real, 0.001);
 
     }
+
     @Test
-    public void TestProductoAdyacente_error_StringCaracterNoNumerico(){
-        String[] arreglo = {"1","3","a","4.2"};
-        String esperado = ("12.6");
-        String real = p1.maximoProductoAdyacente(arreglo);
-        assertEquals(esperado, real);
+    public void TestProductoAdyacente_error_StringCaracterNoNumerico() {
+        String[] arreglo = {"1", "3", "a", "4.2"};
+        double esperado = 12.6;
+        double real = p1.maximoProductoAdyacente(arreglo);
+        assertEquals(esperado, real, 0.001);
 
     }
 }
